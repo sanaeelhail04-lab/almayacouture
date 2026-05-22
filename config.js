d3a51b02010d6734884f5e48ffef0432936acc53
hichamel1 <hicham_elhail@hotmail.com>
2026-05-22 14:45:47 +0100

    Add Supabase and Cloudinary configurations
    Added configuration settings for Supabase and Cloudinary.

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
+// config.js
+const CONFIG = {
+  // Supabase
+  SUPABASE_URL: 'https://ieiujnrgrfsqajcgdiys.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaXVqbnJncmZzcWFqY2dkaXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNDc5MTEsImV4cCI6MjA5NDcyMzkxMX0.ad0ui5P37W-kNaiQk2EYxQv3iA3tWOzHICSNW_VTdvE',
+  
+  // Cloudinary
+  CLOUDINARY_CLOUD_NAME: 'dclli15vh',
+  CLOUDINARY_API_KEY: '969916973571517',
+  CLOUDINARY_API_SECRET: 'xjQHZiOVgM8_l7ql6vUkiyCz8IU',
+  CLOUDINARY_UPLOAD_PRESET: 'almaya_products'
+};
 
+// منع التعديل
+Object.freeze(CONFIG);
